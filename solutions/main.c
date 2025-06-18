5dd18cb3a8c5871a620d075a68a52915b56edbc5
Kai Mischke <kai-timo.mischke@stud.thga.de>
2025-06-18 19:49:13 +0200

    Lösungen zu Task 2

---
+++ solutions/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "flowchart_impl.h"
+
+int main() {
+	    int x;
+
+	        printf("Enter a value for x: ");
+		    scanf("%d", &x);
+
+		        process_value(x);
+
+			    return 0;
+}
